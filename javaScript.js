770d4ebe05e1cdd8ead38fb6ed9554f076ddac68
Axel Calvo Hernández <axelcross2004@gmail.com>
2024-02-06 13:53:17 -0600

    primeros cambios

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+const location1       =      "New York,"   +     "NY";
+const location2="New York,"+"NY";
+const location3= "New York,"   +"NY"
+
+console.log(location1);
+console.log(location2);
+console.log(location3);  
